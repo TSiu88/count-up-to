296ed7dcef1b6723c25494389b03a092ba918f37
Steven Fleming and Tiffany Siu <steven@>
2020-01-22 09:24:23 -0800

    add for loop and show output

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
   $("#form1").submit(function(event){
+    var countBy = parseInt($("#countBy").val());
+    var countTo = parseInt($("#countTo").val());
+    var countingArray = [];
+
+    console.log(countBy);
+    console.log(countTo);
+
+    if( isNaN(countBy)|| isNaN(countTo)){
+      alert("Not a number.  Please enter numbers only.");
+    }
+
+    for (var index = countBy; index <=countTo; index += countBy) {
+      console.log(index);
+      countingArray.push(index);
+    } 
+    $("#numbers").append(countingArray.toString());
+
+    console.log(countingArray);
     
+    $(".output").show();
     event.preventDefault();
   });
 });
